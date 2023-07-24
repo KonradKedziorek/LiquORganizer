81ba170603193d4020fcef1168d2a6762555ebf6
Konrad Kędziorek <konradk453@gmail.com>
2023-07-24 19:47:28 +0200

    Initial - creating repository, creating docker-compose file, connect to the postgres database

---
+++ src/main/java/pl/kedziorek/liquorganizer/LiquORganizerApplication.java
@@ -0,0 +1,13 @@
+package pl.kedziorek.liquorganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquORganizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiquORganizerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/kedziorek/liquorganizer/LiquORganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kedziorek.liquorganizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquORganizerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
